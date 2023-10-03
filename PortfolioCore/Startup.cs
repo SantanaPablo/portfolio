8643ab8e555ff8a6d64bb3afbf388076a7694ee2
perromono <perromono@perromono.ubuntu>
2023-10-02 23:38:13 -0300

    port-02-10-23

---
+++ PortfolioCore/Startup.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.HttpOverrides;
+
+namespace PortfolioCore
+{
+
+
+    public class Startup
+    {
+
+
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+            });
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+
+
+        }
+
+    }
+}
